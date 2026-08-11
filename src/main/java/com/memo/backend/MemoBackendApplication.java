9c6872ac7d1c0982af60ecbaca2e1e6eae5839e4
walong li <3794028003@qq.com>
2026-08-11 11:09:41 +0800

    Initial Spring Boot backend project

---
+++ src/main/java/com/memo/backend/MemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.memo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memo/backend/MemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
